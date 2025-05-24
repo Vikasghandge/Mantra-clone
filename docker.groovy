14db53a19a1f9e8bfffa3502838b7a129bc0d9d6
vikasghandge <ghandgevikas804@gmail.com>
2025-05-24 16:21:54 +0530

    --- Auto Git Commit ---

--- docker.groovy
+++ docker.groovy
@@ -100,7 +100,7 @@ pipeline {
 
         stage('Deploy Docker Locally') {
             steps {
-                sh "docker run -d --name mantra- -p 3000:3000 ${DOCKER_IMAGE}"
+                sh "docker run -d --name mantra-clone -p 3000:3000 ${DOCKER_IMAGE}"
             }
         }
     }
