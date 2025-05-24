59f21351fe1c2e1ac1ebb1db84a2325855d4ac78
vikasghandge <ghandgevikas804@gmail.com>
2025-05-24 16:19:17 +0530

    --- Auto Git Commit ---

--- docker.groovy
+++ docker.groovy
@@ -7,7 +7,7 @@ pipeline {
     }
 
     environment {
-        DOCKER_IMAGE = 'ghandgevikas/star-bucks:latest'
+        DOCKER_IMAGE = 'ghandgevikas/s:latest'
         SCANNER_HOME = tool 'sonar-scanner'
        // CLUSTER_NAME = 'EKS-CLOUD'
        // REGION = 'ap-south-1'
